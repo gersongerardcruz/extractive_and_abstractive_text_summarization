3e99e25306b6e2556ca801397920bd3fc4b3e4fc
Gerson Cruz <gerson.cruz@obf.ateneo.edu>
2022-08-18 20:38:39 +0800

    update python version reqs

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     description='A combination of extractive and abstractive text summarization for summarizing long scientific texts',
     author='Gerson Cruz',
     license='MIT',
-    python_requires="<3.10"
+    python_requires=">=2.7, <3.10"
 )
